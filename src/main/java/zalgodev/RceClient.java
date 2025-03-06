fd0ee7e821f9deeb47dbea07e7407a7fa110e804
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-03-06 06:10:37 +0100

    Main client

---
+++ src/main/java/zalgodev/RceClient.java
@@ -0,0 +1,17 @@
+package zalgodev;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RceClient implements ModInitializer {
+	public static final String MOD_ID = "rceclient";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
